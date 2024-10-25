e20c5f1bc29bff99beb9c05731935faf36cb033e
Reets123 <ibrahimalatig@gmail.com>
2024-10-25 03:28:07 +0000

    commit 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+// Lab 25: Data Structures Races 
+// COMSC-210 - Ibrahim Alatig 
+
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <set>
+#include <string>
+#include <algorithm> // For std::sort
 #include <chrono>
+
 using namespace std;
+using namespace std::chrono;
+
+const int NUM_OF_ELEMENTS = 20000; 
 
 int main() {
     
